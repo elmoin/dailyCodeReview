70686d67d1e806a874f5a09bb19085f6b9f10346
Tomke Reibisch <treibisch@gmail.com>
2016-05-03 21:25:44 +0200

    Well, this is not as easy as it seemed....

--- daily.elm
+++ daily.elm
@@ -1,23 +1,30 @@
 module DailyReview (..) where
 
-import Html
+import Html exposing (Html)
 import Html.Attributes as Attr
 import StartApp.Simple as StartApp
-import Array
+import Array exposing (Array)
 
+main : Signal Html
 main =
   StartApp.start { model = currentTime, view = myView, update = myUpdate }
 
+names : Array String
 names =
-  Array.fromList [  ]
+  Array.fromList [ "Gregor", "Tomke", "Tim", "Jens", "Daniel", "Jonas" ]
 
 port currentTime : Int
 
+myView : a -> Int -> Html
 myView address currentTime =
-  Html.div
-    [ generalStyle ]
-    [ toString currentTime |> Html.text ]
+  let
+    reversedModulo = flip (%)
+  in
+    Html.div
+      [ generalStyle ]
+      [ currentTime |> (flip (%)) (60 * 24) |> reversedModulo (Array.length names) |> toString |> Html.text ]
 
+myUpdate : a -> b -> b
 myUpdate action model = model
 
 generalStyle : Html.Attribute
